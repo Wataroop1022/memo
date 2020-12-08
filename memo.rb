7644c519bd641a22345994df3061a28584442564
EC2 Default User <ec2-user@ip-172-31-7-66.us-east-2.compute.internal>
2020-12-08 07:45:30 +0000

    cleate-memo

--- memo.rb
+++ memo.rb
@@ -1,20 +1,20 @@
 require "csv"
 
-p "1(新規でメモを作成) 2(既存のメモ編集する)"
+puts "1(新規でメモを作成) 2(既存のメモ編集する)"
 memo_type = gets.to_i
 
 if memo_type == 1
-  p ("拡張子を除いたファイル名を入力してください")
+  puts "拡張子を除いたファイル名を入力してください"
   title = gets.chomp
-  p "メモしたい内容を記入してください\n完了したらCtrl + Dを押します"
+  puts "メモしたい内容を記入してください\n完了したらCtrl + Dを押します"
   content = gets.chomp
   CSV.open("#{title}.csv", "w") do |csv| 
     csv << ["#{content}"]
   end 
 elsif memo_type == 2
-  p ("書き換えるファイル名を入力してください")
+  puts "書き換えるファイル名を入力してください"
   filename = gets.chomp
-  p "変更する内容を記入してください\n完了したらCtrl + Dを押します"
+  puts "変更する内容を記入してください\n完了したらCtrl + Dを押します"
   revised_content = gets.chomp
   CSV.open("#{filename}.csv","a") do |csva|
     csva << ["#{revised_content}"]
